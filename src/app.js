8eb60d36d2321b0b33ff2dcc34ef95c7bfe012b6
Haroon Amin <aminharoon699@gmail.com>
2026-02-26 12:50:33 +0530

    feat: enable CORS support in the application

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    credentials: true,
+  })
+);
 
 /**required routes  */
 const authRoute = require("../src/routes/auth.route");
